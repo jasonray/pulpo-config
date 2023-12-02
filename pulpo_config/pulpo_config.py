eadb4b1766a81aca4ae8927f095217456761efb2
Jason Ray <jayray.net@gmail.com>
2023-12-01 19:37:49 -0500

    remove debugging print statements

--- pulpo_config/pulpo_config.py
+++ pulpo_config/pulpo_config.py
@@ -29,21 +29,16 @@ class Config():
 
     def process_args(self, args: dict):
         if args:
-            print(f'processing args [{args}]')
             if isinstance(args, argparse.ArgumentParser):
                 args = args.parse_args()
-                print(f'process command line arguments [{args}]')
             if isinstance(args, argparse.Namespace):
                 args = vars(args)
-                print(f'converted args to dictionary [{args}]')
 
             for arg in args:
                 print(f'processing args [arg={arg}]')
                 # value = getattr(args, arg)
                 value = args.get(arg)
-                print(f'processing args [arg={arg}][value={value}]')
                 if value:
-                    print(f'set config [key={arg}][value={value}]')
                     self.set(arg, value)
 
     def _load_options_from_file(self, json_file_path: str = None) -> dict:
@@ -73,18 +68,12 @@ class Config():
         return value
 
     def _get_handle_env(self, value):
-        print(f'_get_handle_env 1 => {value=}')
         Environment_Variable_Prefix = '$ENV.'
         if value:
-            print(f'_get_handle_env 2 => {value=}')
             if isinstance(value, str):
-                print(f'_get_handle_env 3 => {value=}')
                 if value.startswith(Environment_Variable_Prefix):
-                    print(f'_get_handle_env 4 => {value=}')
                     env_var_key = value[len(Environment_Variable_Prefix):]
                     value = os.getenv(env_var_key)
-                    print(f'_get_handle_env 5 => {value=}')
-        print(f'_get_handle_env 6 => {value=}')
         return value
 
     def getAsBool(self, key: str, default_value: typing.Any = None) -> bool:
@@ -106,24 +95,14 @@ class Config():
 
     # support key=a.b.c where it will create intermediate dictionaries
     def set(self, key: str, value: typing.Any):
-        print('options.set')
         keys = key.split('.')
 
         parent = self.__options
-        print('options', self.__options)
-        print(f'keys [keys:{keys}][key count:{len(keys)}]')
         for key_number in range(0, len(keys) - 1):
             key = keys[key_number]
-            print(f'iterate keys [key_num:{key_number}][key={key}][parent={parent}]')
             if not key in parent:
-                print(f'init item [key={key}][parent={parent}]')
                 parent[key] = {}
-                print(f'init item complete [key={key}][parent={parent}]')
             parent = parent.get(key)
-            print(f'new parent [parent={parent}]')
-            print('options l', self.__options)
 
         last_key = keys[len(keys) - 1]
-        print(f'set parent to value [parent={parent}][last_key={last_key}][value={value}]')
         parent[last_key] = value
-        print('options', self.__options)
